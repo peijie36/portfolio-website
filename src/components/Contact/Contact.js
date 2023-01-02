ee6e96d90b9800cae3d2a39be2e20029dc7a7962
Peijie Zheng <peijiezheng2001@gmail.com>
2023-01-01 21:20:23 -0800

    Update contact form style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,42 @@
 import "./Contact.css";
+import {useState} from 'react'
 
 const Contact = () => {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [complete, setComplete] = useState(false);
+
+  // const isFormValid = () => {
+  //   if(firstName != "" && lastName != "" && email != "" && subject != "" && message != "") {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.id]: event.target.value });
+    if(state.firstName != "" && state.lastName != "" && state.email != "" && state.subject != "" && state.message != "") {
+        setComplete(true);
+        document.querySelector("#submit").classList.add("complete");
+    } else{
+      setComplete(false);
+      if(document.querySelector("#submit").classList.contains("complete")) {
+        document.querySelector("#submit").classList.remove("complete");
+      }
+    }
+  };
+
+  const handleSubmit = (event) => {
+    alert("Your message is successfully sent!");
+    event.preventDefault();
+    setState({ firstName: "", lastName: "", email: "", subject: "", message: ""});
+  }
+
   return (
     <div className="contact" id="contact">
       <h2 className="contact__title">Contact Me</h2>
@@ -8,29 +44,32 @@ const Contact = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus sem
         malesuada, sollicitudin dui in, eleifend augue.
       </p>
-      <form className="contact__form">
+      <form className="contact__form" onChange={handleChange} onSubmit={handleSubmit}>
         <div>
           <section className="full__name">
             <div className="name">
               <label for="first">First name</label>
-              <input type="text" id="first" required></input>
+              <input type="text" id="firstName" value={state.firstName} required></input>
             </div>
             <div className="name">
               <label for="last">Last name</label>
-              <input type="text" id="last" required></input>
+              <input type="text" id="lastName" value={state.lastName}  required></input>
             </div>
           </section>
         </div>
         <label for="email">Email</label>
-        <input type="email" id="email" required></input>
+        <input type="email" id="email" value={state.email}  required></input>
+
         <label for="subject">Subject</label>
-        <input type="text" id="subject" required></input>
+        <input type="text" id="subject" value={state.subject}  required></input>
+        
         <label for="message">Message</label>
         <textarea
+          value={state.message}
           id="message"
           rows="7"
         ></textarea>
-        <button type="submit" className="submit__btn">Submit</button>
+        <button type="submit" id="submit" className="submit__btn" disabled={!complete}>Submit</button>
       </form>
     </div>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,18 +7,18 @@ const Projects = () => {
       <h2 className="project__header" >Projects</h2>
       <div className="project__grid">
         <div className="project">
-            <h3 className="project__title">Moments</h3>
+            <h3 className="project__title">Title</h3>
             <p className="project__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus sem malesuada, sollicitudin dui in, eleifend augue. Nam tortor diam, luctus in massa vitae, suscipit imperdiet odio. Phasellus fringilla mattis nulla at ultricies. Sed vel dui a massa hendrerit ultrices.</p>
             <a href="https://github.com/peijie36" target="_blank" className="github"><AiFillGithub style={{fill: "black"}}/></a>
         </div>
         <div className="project">
-            <h3 className="project__title">Moments</h3>
+            <h3 className="project__title">Title</h3>
             <p className="project__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus sem malesuada, sollicitudin dui in, eleifend augue. Nam tortor diam, luctus in massa vitae, suscipit imperdiet odio. Phasellus fringilla mattis nulla at ultricies. Sed vel dui a massa hendrerit ultrices.</p>
             <a href="https://github.com/peijie36" target="_blank" className="github"><AiFillGithub style={{fill: "black"}}/></a>
         </div>
         
         <div className="project">
-            <h3 className="project__title">Moments</h3>
+            <h3 className="project__title">Title</h3>
             <p className="project__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus sem malesuada, sollicitudin dui in, eleifend augue. Nam tortor diam, luctus in massa vitae, suscipit imperdiet odio. Phasellus fringilla mattis nulla at ultricies. Sed vel dui a massa hendrerit ultrices.</p>
             <a href="https://github.com/peijie36" target="_blank" className="github"><AiFillGithub style={{fill: "black"}}/></a>
         </div>
